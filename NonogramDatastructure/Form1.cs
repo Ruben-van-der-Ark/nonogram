0a1c17820b81a41e9ff40eaca10ec088deda72b1
rsvdark <s1210076@student.windesheim.nl>
2025-03-27 11:30:25 +0100

    Row labels

--- NonogramDatastructure/Form1.cs
+++ NonogramDatastructure/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Drawing;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
-using System.Runtime.CompilerServices;
 
 // TODO: Fix twee verschillende comment talen
 
@@ -15,14 +9,15 @@ namespace NonogramDatastructure
 {
     public partial class Form1 : Form
     {
-        // Definieer nodige waardes zodat ze in de goede scope staan
-        bool[,] answer;
+        // Define the necessary variables
+        bool[,] solution;
         int cellsize;
-        int gridX = 400;
+        int gridX = 450;
         int gridY = 50;
         int blackchance = 3;
         int cols;
         int rows;
+
 
         public Form1()
         {
@@ -31,8 +26,8 @@ namespace NonogramDatastructure
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            // Als er nog geen grid is gegenereerd, probeer het dan ook niet te painten
-            if (answer is null)
+            // If there isn't a grid, don't try painting it
+            if (solution is null)
             {
                 return;
             }
@@ -40,44 +35,59 @@ namespace NonogramDatastructure
             // Definieer twee kleuren
             SolidBrush blackBrush = new SolidBrush(Color.Black);
             SolidBrush whiteBrush = new SolidBrush(Color.White);
-            
-            // Ga langs elke cel in het grid
+
+            // Go through every cell in the grid
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    // Definieer de positie en grootte van de rectangle
+                    // Define the place and size of the rectangle
                     RectangleF rect = new RectangleF(i * cellsize + gridX, j * cellsize + gridY, cellsize, cellsize);
 
-                    // Paint de rectangle met de goede kleur (black voor true, white voor false)
-                    e.Graphics.FillRectangle(answer[i, j] ? blackBrush : whiteBrush, rect);
+                    // Paint the rectangle with the correct color (black for true, white for false)
+                    e.Graphics.FillRectangle(solution[i, j] ? blackBrush : whiteBrush, rect);
                 }
             }
 
+            // Columns
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.Alignment = StringAlignment.Center;
             for (int i = 0; i < rows; i++)
             {
                 string values = string.Join(Environment.NewLine, GetColLabelNumbers(i));
-                DrawString(i*cellsize +  gridX, rows + cellsize * (rows)+5, values);
+                DrawString(i * cellsize + gridX, 
+                    gridY + rows * cellsize,
+                    values.Normalize(), drawFormat);
+            }
+            drawFormat = new StringFormat();
+            for (int i = 0; i < cols; i++)
+            {
+                string values = string.Join(", ", GetRowLabelNumbers(i)).Trim();
+                float stringWidth = GetTextWidth(values);
+                DrawString(gridX - stringWidth - 10,
+                    gridY + i * cellsize,
+                    values,
+                    drawFormat);
             }
         }
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
-            // Maak een 2D bool array om het antwoord in op te slaan
-            // Alles staat standaard op false
-            answer = new bool[(int) numRows.Value, (int) numCols.Value];
-            cellsize = 250/((int) numRows.Value);
-            rows = (int) numRows.Value;
-            cols = (int) numCols.Value;
-            // Instantieer een Random class om een willekeurig patroon te kunnen 
+            // Make a boolean array to store the solution
+            // Every boolean defaults to false
+            rows = (int)numRows.Value;
+            cols = (int)numCols.Value;
+            solution = new bool[rows, cols];
+            cellsize = 250 / (rows);
+            // Instantiate a Random class to generate random numbers
             Random rand = new Random();
 
-            // Ga langs elke waarde en geef elke coordinaat een willekeurige waarde
+            // Go through every cell and pick a random boolean value
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    answer[i, j] = (rand.Next(blackchance) > 0); // 25/75 voor true of false
+                    solution[i, j] = (rand.Next(blackchance) > 0); // 25/75 for true or false
                 }
             }
             this.Refresh();
@@ -92,7 +102,7 @@ namespace NonogramDatastructure
 
             for (int i = 0; i < cols; i += 1)
             {
-                if (answer[row, i])
+                if (solution[row, i])
                 {
                     total[idx] += 1; // If 1, add 1 to the count
                 }
@@ -119,7 +129,7 @@ namespace NonogramDatastructure
 
             for (int i = 0; i < rows; i += 1)
             {
-                if (answer[i, col])
+                if (solution[i, col])
                 {
                     total[idx] += 1; // If 1, add 1 to the count
                 }
@@ -136,17 +146,47 @@ namespace NonogramDatastructure
             return total.ToArray();
         }
 
-        public void DrawString(float x, float y, string text)
+        public void DrawString(float x, float y, string text, StringFormat drawFormat)
         {
             Graphics formGraphics = this.CreateGraphics();
-            Font drawFont = new Font("Arial", 200/cellsize);
+            Font drawFont = new Font("Consolas", 0.66f * cellsize);
             SolidBrush drawBrush = new SolidBrush(Color.Black);
-            StringFormat drawFormat = new StringFormat();
-            drawFormat.Alignment = StringAlignment.Center;
-            formGraphics.DrawString(text, drawFont, drawBrush, x+cellsize/2, y, drawFormat);
+            formGraphics.DrawString(text, drawFont, drawBrush, x + cellsize / 2, y, drawFormat);
             drawFont.Dispose();
             drawBrush.Dispose();
             formGraphics.Dispose();
         }
+
+        public float GetTextWidth(string text)
+        {
+            // If there isn't a cellsize yet, that means we can't calculate the font size.
+            if (cellsize == 0)
+            {
+                MessageBox.Show("Character width calculation error");
+                return 0;
+            }
+            Graphics g = CreateGraphics();
+            Font drawFont = new Font("Consolas", 0.66f * cellsize);
+            SizeF size = g.MeasureString(text, drawFont);
+            float charWidth = size.Width; // Width in pixels
+            g.Dispose();
+            return charWidth;
+        }
+        public float GetTextHeight(string text)
+        {
+            // If there isn't a cellsize yet, that means we can't calculate the font size.
+            if (cellsize == 0)
+            {
+                MessageBox.Show("Character height calculation error");
+                return 0;
+            }
+            Graphics g = CreateGraphics();
+            Font drawFont = new Font("Consolas", 0.66f * cellsize);
+            SizeF size = g.MeasureString(text, drawFont);
+            float charHeight = size.Height; // Width in pixels
+            g.Dispose();
+            return charHeight;
+        }
+
     }
 }
